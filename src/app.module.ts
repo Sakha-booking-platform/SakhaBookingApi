f8d48f6ae51d15f3566178dcbadcf96830985b5e
Bashar <bashargalal77@gmail.com>
2026-05-17 23:19:26 +0300

    merge profile feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { ProfilesModule } from './profiles/profiles.module';
 
-@Module({
+import { AppController } from './app.controller';
+
+@Module({ 
   imports: [
       ConfigModule.forRoot({
       isGlobal: true,
@@ -16,3 +17,4 @@ import { ProfilesModule } from './profiles/profiles.module';
   providers: [AppService],
 })
 export class AppModule {}
+// trigger recompile
